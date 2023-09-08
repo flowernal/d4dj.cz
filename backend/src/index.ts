bcfc8dfcd101e64e114f94ee2ca80d511943a278
CutoNaito <cutonaito@github.com>
2023-09-09 00:58:52 +0200

    tohle nebude fungovat

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
+import { cookie } from "@elysiajs/cookie";
+import { jwt } from "@elysiajs/jwt";
 import { createAttachment, createPost, createUser, getAttachment, getAttachments, getAttachmentsByPostId, getPost, getPosts, getUser, getUsers, login } from "./routes";
 
 const app = new Elysia()
+    .use(
+        jwt({
+            name: "jwt",
+            secret: ""
+        })
+    )
+    .use(cookie())
+
     .get("/api/posts", getPosts)
     .get("/api/posts/:id", ({ params: { id } }) => getPost(id))
     .post("/api/posts", ({ body }) => createPost(body))
@@ -9,7 +19,18 @@ const app = new Elysia()
     .get("/api/users", getUsers)
     .get("/api/users/:id", ({ params: { id } }) => getUser(id))
     .post("/api/users", ({ body }) => createUser(body))
-    .post("/api/users/login/:id", ({ params: { id }, body }) => login(id, body))
+    .post("/api/users/login/:id", async ({ params: { id }, body, jwt, cookie, setCookie, params }) => {
+        if ((await login(id, body)).success) {
+            setCookie("auth", await jwt.sign(params), {
+                httpOnly: true,
+                maxAge: 7 * 86400,
+            })
+
+            return { success: true, cookie: cookie.auth };
+        };
+
+        return { success: false, error: "Something went wrong." };
+    })
     
     .get("/api/attachments", getAttachments)
     .get("/api/attachments/post/:id", ({ params: { id } }) => getAttachmentsByPostId(id))
