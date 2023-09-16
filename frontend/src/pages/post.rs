763010ee6a44c1a3122ee7f855c4afec375b7dd2
CutoNaito <cutonaito@github.com>
2023-09-16 18:08:10 +0200

    funguje postovani getrekt @Akane Yabushima (薮島朱音)

--- frontend/src/pages/post.rs
+++ frontend/src/pages/post.rs
@@ -2,6 +2,7 @@ use yew::prelude::*;
 use gloo_net::http::{Request, Response};
 use gloo::console::log;
 use serde::{Serialize, Deserialize};
+use serde_json::json;
 use crate::components::{
     post::post_form::{Post, PostProps},
     header::Header,
@@ -12,20 +13,38 @@ use crate::components::{
 struct PostData {
     title: String,
     body: String,
-    user_id: String
+    user_id: u32
+}
+
+#[derive(Serialize, Deserialize)]
+struct PostResponse {
+    success: bool
 }
 
 #[function_component(PostPage)]
 pub fn post_page() -> Html {
     let handle_submit = Callback::from(|data: PostProps| {
-        let post_data = PostData {
-            title: data.title,
-            body: data.body,
-            user_id: data.user_id
-        };
+        let post_data = json!({
+            "title": data.title,
+            "body": data.body,
+            "user_id": data.user_id.parse::<u32>().unwrap()
+        });
 
-        let serialized_post_data = serde_json::to_string(&post_data).unwrap();
+        wasm_bindgen_futures::spawn_local(async move {
+            log!(post_data.to_string());
+            let response = Request::post("https://d4dj.cz/api/posts")
+                .header("Content-Type", "application/json")
+                .body(post_data.to_string())
+                .unwrap()
+                .send()
+                .await
+                .unwrap()
+                .json::<PostResponse>()
+                .await
+                .unwrap();
 
+            log!(response.success);
+        });
     });
 
     html! {
